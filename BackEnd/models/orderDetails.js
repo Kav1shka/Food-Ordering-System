27e08225d8771f0bfbced7fe63bccfeed50de936
Kav1shka <103574964+Kav1shka@users.noreply.github.com>
2022-08-20 15:59:36 +0530

    update 2022.8.20

--- BackEnd/models/orderDetails.js
+++ BackEnd/models/orderDetails.js
@@ -2,21 +2,68 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
-    name: {
+    CustomerName: {
       type: String,
       required: true,
     },
+
+    CustomerType:{
+      type:String,
+      required:true,
+    },
+
     email: {
       type: String,
       required: true,
     },
+
     foodName: {
       type: [],
       required: true,
     },
-    address: {
+
+    foodCode:{
+      type:String,
+      required:true,
+    },
+
+    Faculty: {
       type: String,
       required: true,
+    },
+
+    paymentMethod:
+    {type:String,
+      required:true
+    },
+
+    itemsPrice:{
+      type:Number,
+      required:true
+    },
+
+    totalprice:{
+      type:Number,
+      required:true
+    },
+
+    //userId:{type:moongoose.Schema.Types.ObjectId,ref:'User',required:true},
+    isPaid:{
+      type:Boolean,
+      default:false
+    },
+
+    paidAt:{
+      type:Date
+    },
+
+    isPlaced:{
+      type:Boolean,
+      default:true
+    },
+    
+    paymentId:{
+       type:String,
     },
   },
   { timestamps: true }
--- BackEnd/models/products.js
+++ BackEnd/models/products.js
@@ -1,33 +1,45 @@
 const mongoose = require("mongoose");
 
 const foodSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    category: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    cost: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    description: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    image: {
+{
+  name: {
       type: String,
       required: true,
     },
+  category: {
+      type: String,
+      required: true,
+    },
+  cost: {
+      type: String,
+      required: true,
+    },
+  rating:{
+      type: String,
+      required: true,
+    },
+  description: {
+      type: String,
+      required: true,
+    },
+  type:{
+      type:String,
+      required:true,
   },
-  { timestamps: true }
+  price:{
+      type:Number,
+      required:true,
+  },
+  image:{
+      type: String,
+      required: true,
+  },
+    countInStock:{
+    type:Number,
+    required:true
+  }
+  },{ timestamps: true }
 );
 
 module.exports = mongoose.model("products", foodSchema);
+export default products;
